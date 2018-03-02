13c93dc6b58dd552ae65bacba2d117aea105f702
victoriabozhko <victoria.bozhko@gmail.com>
2018-03-02 12:01:18 +0300

    results grouped to 7 nums inline; writing to csv ongoing

--- src/GrabHtml.java
+++ src/GrabHtml.java
@@ -8,13 +8,36 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 import java.io.*;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class GrabHtml {
 
     private static String url = "https://www.euro-jackpot.net/en/results-archive-";
     private static String year;
+
+    private static final char DEFAULT_SEPARATOR = ',';
+
+    static String csvFile = "test.csv";
+    static FileWriter writer;
+
+    static {
+        try {
+            writer = new FileWriter(csvFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        getNumbers(htmlString("2018"));
+//        getNumbers(htmlString("2017"));
+//        getNumbers(htmlString("2016"));
+//        getNumbers(htmlString("2015"));
+//        getNumbers(htmlString("2014"));
+//        getNumbers(htmlString("2013"));
+//        getNumbers(htmlString("2012"));
+
+    }
 
     private static String htmlString(String year) throws IOException {
         GrabHtml.year = year;
@@ -28,8 +51,10 @@ public class GrabHtml {
         }
 
         in.close();
+
         int start = String.valueOf(stringBuilder).indexOf("<tbody>");
         int end = String.valueOf(stringBuilder).indexOf("</tbody>");
+
         String result = String.valueOf(stringBuilder).substring(start, end + 8).replaceAll("\\s{2,}", "");
 
         BufferedWriter bwr = new BufferedWriter(new FileWriter(new File(year + ".xml")));
@@ -39,48 +64,37 @@ public class GrabHtml {
         return result;
     }
 
-    public static void main(String[] args) throws IOException {
-        getNumbers(htmlString("2018"));
-        getNumbers(htmlString("2017"));
-        getNumbers(htmlString("2016"));
-        getNumbers(htmlString("2015"));
-        getNumbers(htmlString("2014"));
-        getNumbers(htmlString("2013"));
-        getNumbers(htmlString("2012"));
-
-    }
-
-    public static void getNumbers(String xmlRecords) {
+    public static void getNumbers(String xmlString) {
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
             InputSource is = new InputSource();
-            is.setCharacterStream(new StringReader(xmlRecords));
-
+            is.setCharacterStream(new StringReader(xmlString));
             Document doc = db.parse(is);
-            int s = doc.getElementsByTagName("ul").getLength();
+
+            int weeks = doc.getElementsByTagName("ul").getLength();
+
             XPathFactory xPathfactory = XPathFactory.newInstance();
             XPath xpath = xPathfactory.newXPath();
-            List<String> regular2017 = new ArrayList<>();
-            List<String> euro2017 = new ArrayList<>();
 
-            System.out.println("\n" + year + " regular numbers are: ");
-            for (int i = 1; i <= s; i++) {
+            System.out.println("\n" + year + " numbers are: ");
+            for (int i = 1; i <= weeks; i++) {
                 for (int j = 1; j <= 5; j++) {
-                    XPathExpression expr = xpath.compile("((//ul)[" + String.valueOf(i) + "]/li[@class='ball']/span)[" + j + "]");
-                    regular2017.add(expr.evaluate(doc));
-                }
-            }
-            System.out.println(regular2017);
 
-            System.out.println("\n" + year + " euro numbers are: ");
-            for (int i = 1; i <= s; i++) {
+
+                    XPathExpression expr = xpath.compile("((//ul)[" + String.valueOf(i) + "]/li[@class='ball']/span)[" + j + "]");
+
+                    writer.append(expr.evaluate(doc));
+                    System.out.print(expr.evaluate(doc) + " ");
+                }
                 for (int j = 1; j <= 2; j++) {
                     XPathExpression expr = xpath.compile("((//ul)[" + String.valueOf(i) + "]/li[@class='euro']/span)[" + j + "]");
-                    euro2017.add(expr.evaluate(doc));
+                    writer.append(expr.evaluate(doc));
+                    System.out.print(expr.evaluate(doc) + " ");
                 }
+                System.out.print("\n");
+
             }
-            System.out.println(euro2017);
 
         } catch (Exception e) {
             e.printStackTrace();
